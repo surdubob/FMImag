6e99d8c5fe6a676c476235b50a66339ebda69f44
Varzan Laurentiu <varzan.laurentiu@gmail.com>
2023-01-03 23:12:04 +0200

    firt frontend code for products page

--- FMImag-frontend/src/app/components/products/products.component.ts
+++ FMImag-frontend/src/app/components/products/products.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../../services/product.service";
+import {Product} from "../../dto/product";
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.listProducts();
+  }
+
+  listProducts()
+  {
+    this.productService.getProductList().subscribe(
+        (data: any[]) => {
+        this.products = data;
+      }
+    )
+  }
 }
