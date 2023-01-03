5be115067b0e7bfa9fc6f3640fdb208efa5d6751
Varzan Laurentiu <varzan.laurentiu@gmail.com>
2023-01-03 23:13:49 +0200

    controllers for categories and filters v1.0

--- FMImag-API/Controllers/FiltersController.cs
+++ FMImag-API/Controllers/FiltersController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FMImag.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FMImag.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class FiltersController : Controller
     {
 
+        private FmiDBContext dbContext;
 
-
+        public FiltersController(FmiDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
     }
 }
